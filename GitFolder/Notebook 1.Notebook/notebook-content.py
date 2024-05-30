fdb2ae8810dde128cf02db7364840c9cdef48497
JerryMS1 <jiat@microsoft.com>
2024-05-30 20:29:02 +0800

    Committing 1 item from workspace cd0ebbc8-297f-4c4d-a4bd-b267e2a8403a

--- GitFolder/Notebook 1.Notebook/notebook-content.py
+++ GitFolder/Notebook 1.Notebook/notebook-content.py
@@ -5,4 +5,11 @@
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
+ 
 
+# CELL ********************
+
+import pandas as pd
+# Load data into pandas DataFrame from f"{mssparkutils.nbResPath}/builtin/1.5million_records_small.csv"
+df = pd.read_csv(f"{mssparkutils.nbResPath}/builtin/1.5million_records_small.csv")
+display(df)
